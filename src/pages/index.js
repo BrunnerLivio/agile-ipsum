e38f1facf4053c6180d3e6fdd146266a284589cc
tyb-dev <57002348+tyb-dev@users.noreply.github.com>
2019-10-25 15:32:35 +0200

    Update index.js
    Case insensitive de-duplication

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = ({ data }) => {
     count,
     words,
     format: 'html'
-  }).replace(/\b(\w+)(\s+\1)+\b/g, "$1"); // de-duplicate words
+  }).replace(/\b(\w+)(\s+\1)+\b/ig, "$1"); // de-duplicate words
   
   return (
     <Layout>
