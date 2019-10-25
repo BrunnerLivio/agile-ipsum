72d7738168f47fae1358eef08de78f022a82923c
Livio Brunnner <livio.brunner.lb1@gmail.com>
2019-10-25 16:14:20 +0200

    Check programmatically the min / max paragraph value

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,36 +7,53 @@ import { loremIpsum } from "lorem-ipsum"
 const IndexPage = ({ data }) => {
   const words = data.site.siteMetadata.words
 
-  let count = 3;
+  let error = ''
+  let count = 3
+  const maxParagraphs = 50
+  const minParagraphs = 1
+
   const [paragraphs, setParagraphs] = useState(count)
+
   if (!isNaN(paragraphs) && parseInt(paragraphs) > 0) {
-    count = parseInt(paragraphs);
+    count = parseInt(paragraphs)
   }
+
+  if(paragraphs > maxParagraphs) {
+    error = <div class="error">Sorry, {maxParagraphs} is the maximum</div>
+  }
+
+  if(paragraphs < minParagraphs) {
+    error = <div class="error">Sorry, {minParagraphs} is the minimum</div>
+  }
+
   const ipsum = loremIpsum({
     units: "paragraphs",
     count,
     words,
-    format: 'html'
-  }).replace(/\b(\w+)(\s+\1)+\b/ig, "$1"); // de-duplicate words
-  
+    format: "html",
+  })
+    // de-duplicate words
+    .replace(/\b(\w+)(\s+\1)+\b/gi, "$1")
+
   return (
     <Layout>
       <SEO title="Home" />
       <form>
         <div>
-          <label  htmlFor="paragraphs">Paragraphs:</label>
+          {error}
+          <label htmlFor="paragraphs">Paragraphs:</label>
           <input
             class="paragraphs"
             name="paragraphs"
-            min="1"
-            max="10000"
+            min={minParagraphs}
+            max={maxParagraphs}
             type="number"
             value={paragraphs}
             onChange={e => setParagraphs(e.target.value)}
           />
         </div>
       </form>
-      <div class="ipsum" dangerouslySetInnerHTML={{__html: ipsum}} />
+      <div class="ipsum" dangerouslySetInnerHTML={{ __html: ipsum }} />
     </Layout>
   )
 }
