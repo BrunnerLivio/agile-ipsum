787179889bbc610293aa0821dc6bc8caf704d4ea
Daniel Heppner <daniel.heppner@roche.com>
2019-10-25 17:22:00 +0200

    Add agile catchphrases

--- content/site-metadata.js
+++ content/site-metadata.js
@@ -1,7 +1,9 @@
 const words = require("./word-parser")
+const dict = require("./dict")
 
 module.exports = {
   words,
+  dict,
   title: `Agile Ipsum`,
   description: `Generate an Agile Lorem Ipsum text`,
   author: `@brunnerlivio`,
