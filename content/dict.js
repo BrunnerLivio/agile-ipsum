8f789d057637d9fe197a789d5f29f285ac5de1fd
Andrin Eugster <andrin.eugster@roche.com>
2021-03-02 13:40:26 +0100

    Add term produce to dict

--- content/dict.js
+++ content/dict.js
@@ -65,7 +65,8 @@ const dict = {
         "improvise",
         "adapt",
         "overcome",
-        "generate"
+        "generate",
+        "produce"
     ],
     "adjective": [
         "agile",
